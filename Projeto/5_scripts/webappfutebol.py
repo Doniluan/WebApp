cd9f22c5f418860f94136cb1f7813e6d4463e70f
luan <luan_zeti@hotmail.com>
2024-10-08 16:06:59 -0300

    k

--- Projeto/5_scripts/webappfutebol.py
+++ Projeto/5_scripts/webappfutebol.py
@@ -1,63 +1,41 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime
-import os
-
-
+from datetime import date
 
 st.write(
     """
-    **Futebol Web App**
+    **Futebol Wep App**
     """
 )
 
 st.sidebar.header('Escolha os times')
 
-# Função para carregar os dados
 def get_data():
-    base_path = r'C:\Users\luan_\Documents\LU\CURSOS\Web Data Applications\WebApp\Projeto\2_Bases_Tratadas'
-    file_name = 'futebol2.csv'
-    path = os.path.join(base_path, file_name)
+    path = './2_bases_tratadas/futebol2.csv'
+    return pd.read_csv(path, sep=';')
 
-    st.write(f"Usando o caminho: {path}")  # Adicione esta linha para ver o caminho
-    ...
-
-# Carregar os dados
 df = get_data()
+df_data = pd.to_datetime(
+    df['datanova']).dt.date.drop_duplicates()
 
-# Obter as datas únicas no formato datetime
-df['datanova'] = pd.to_datetime(df['datanova'])
-df_data = df['datanova'].dt.date.drop_duplicates()
-
-# Definir as datas mínima e máxima
 min_data = min(df_data)
 max_data = max(df_data)
-
-# Obter a lista de times
 times = df['home_team_name'].drop_duplicates()
 
-# Entradas do usuário para datas e time
-start_date = st.sidebar.date_input("Digite uma data de início:", min_data)
-end_date = st.sidebar.date_input("Digite uma data final:", max_data)
+start_date = st.sidebar.text_input(
+    "Digite uma data de inicio:", min_data) 
+end_date = st.sidebar.text_input(
+    "Digite uma data final:", max_data) 
 
-# Garantir que a data de início seja antes da data final
-if start_date > end_date:
-    st.sidebar.error('A data de início deve ser anterior à data final.')
+time_escolhido = st.selectbox(
+    "Escolha o time", times)
 
-# Escolher o time
-time_escolhido = st.sidebar.selectbox("Escolha o time", times)
+df = df[(df['home_team_name'] == time_escolhido) 
+    & (pd.to_datetime(df['datanova']) >= start_date) 
+    & (pd.to_datetime(df['datanova']) <= end_date)]
 
-# Filtrar o DataFrame com base nas escolhas do usuário
-df_filtered = df[(df['home_team_name'] == time_escolhido) 
-                 & (df['datanova'].dt.date >= start_date) 
-                 & (df['datanova'].dt.date <= end_date)]
-
-# Exibir informações no app
 st.header('Time: ' + time_escolhido.upper())
 st.write('Gols em casa')
-
-# Verificar se há dados filtrados antes de exibir o gráfico
-if not df_filtered.empty:
-    st.line_chart(data=df_filtered, x='datanova', y='home_team_goal_count')
-else:
-    st.write("Nenhum jogo encontrado no intervalo de datas selecionado.")
+st.line_chart(data=df, 
+              x='datanova', 
+              y='home_team_goal_count')
